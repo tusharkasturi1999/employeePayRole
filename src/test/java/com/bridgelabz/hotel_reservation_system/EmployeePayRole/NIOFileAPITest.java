f320e49c0e838f69e782088594a08f032f966e5d
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-23 11:11:44 +0530

    Added: watchService class

--- src/test/java/com/bridgelabz/hotel_reservation_system/EmployeePayRole/NIOFileAPITest.java
+++ src/test/java/com/bridgelabz/hotel_reservation_system/EmployeePayRole/NIOFileAPITest.java
@@ -39,11 +39,19 @@ public class NIOFileAPITest {
 			}
 			Assert.assertTrue(Files.exists(tempFile));
 		});
-		
+
 		//List files ,directories as well as fies with extension
 		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
-
 	}
+
+	@Test
+	public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException{
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
+	}
+
+
 }
